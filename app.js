3ce9bd46f6059c0dea23f3c66766fa8a38a674b2
Gastonm1 <gaston.marques@gmail.com>
2021-10-04 16:35:18 -0500

    randomize data

---
+++ app.js
@@ -0,0 +1,39 @@
+//Grab some stuff
+const section = document.querySelector("section");
+const playerLivesCount = document.querySelector("span");
+const playerLives = 4;
+
+//Link player Lives count
+playerLivesCount.textContent = playerLives;
+
+//Generate the data used for the cards
+// Requirements: Images & Name for Cards
+//We generate the object 🧑🏻‍💻
+const getData = () => [
+  { imgSrc: "./images/beatles.jpeg", id: 1, name: "beatles" },
+  { imgSrc: "./images/blink182.jpeg", id: 2, name: "blink 182" },
+  { imgSrc: "./images/fkatwigs.jpeg", id: 3, name: "fka twigs" },
+  { imgSrc: "./images/fleetwood.jpeg", id: 4, name: "fleetwood" },
+  { imgSrc: "./images/joy-division.jpeg", id: 5, name: "joy division" },
+  { imgSrc: "./images/ledzep.jpeg", id: 6, name: "lep zeppelin" },
+  { imgSrc: "./images/metallica.jpeg", id: 7, name: "metallica" },
+  { imgSrc: "./images/pinkfloyd.jpeg", id: 8, name: "pink floyd" },
+  { imgSrc: "./images/beatles.jpeg", id: 9, name: "beatles" },
+  { imgSrc: "./images/blink182.jpeg", id: 10, name: "blink 182" },
+  { imgSrc: "./images/fkatwigs.jpeg", id: 11, name: "fka twigs" },
+  { imgSrc: "./images/fleetwood.jpeg", id: 12, name: "fleetwood" },
+  { imgSrc: "./images/joy-division.jpeg", id: 13, name: "joy division" },
+  { imgSrc: "./images/ledzep.jpeg", id: 14, name: "led zeppelin" },
+  { imgSrc: "./images/metallica.jpeg", id: 15, name: "metallica" },
+  { imgSrc: "./images/pinkfloyd.jpeg", id: 16, name: "pink floyd" },
+];
+
+// Randomize the cards
+const randomize = () => {
+  const cardData = getData();
+  //All you need to randomize array
+  cardData.sort(() => Math.random() - 0.5);
+  console.log(cardData);
+};
+
+randomize();

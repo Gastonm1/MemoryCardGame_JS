4cb8490b9b0fee98ed88436a1ff7b3fa52d7a8f7
Gastonm1 <gaston.marques@gmail.com>
2021-10-04 16:44:32 -0500

    generate new divs for each item within dataset

--- app.js
+++ app.js
@@ -33,7 +33,27 @@ const randomize = () => {
   const cardData = getData();
   //All you need to randomize array
   cardData.sort(() => Math.random() - 0.5);
-  console.log(cardData);
+  return cardData;
 };
 
-randomize();
+// Card Generator Function
+const cardGenerator = () => {
+  const cardData = randomize();
+  //Generate the HTML
+  cardData.forEach((item) => {
+    const card = document.createElement("div");
+    const face = document.createElement("img");
+    const back = document.createElement("div");
+    card.classList = "card";
+    face.classlist = "face";
+    back.classList = "back";
+    //Attach the info to the cards
+    face.src = item.imgSrc;
+    //Attach cards to the section
+    section.appendChild(card);
+    card.appendChild(face);
+    card.appendChild(back);
+  });
+};
+
+cardGenerator();
